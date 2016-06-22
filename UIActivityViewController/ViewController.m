8d801433ae4e4c03ea14fbc363d642e956a9f98e
susheel <susheel455@gmail.com>
2016-06-22 17:08:44 +0530

    just added sample code for inbuilt apps

--- UIActivityViewController/ViewController.m
+++ UIActivityViewController/ViewController.m
@@ -24,4 +24,30 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)shareButton:(id)sender {
+    
+    NSString *textToShare = @"Look at this awesome website for aspiring iOS Developers!";
+    NSURL *myWebsite = [NSURL URLWithString:@"http://www.codingexplorer.com/"];
+    
+    NSArray *objectsToShare = @[textToShare, myWebsite];
+    
+    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:objectsToShare applicationActivities:nil];
+    
+    NSArray *excludeActivities = @[UIActivityTypeAirDrop,
+                                   UIActivityTypePrint,
+                                   UIActivityTypeAssignToContact,
+                                   UIActivityTypeSaveToCameraRoll,
+                                   UIActivityTypeAddToReadingList,
+                                   UIActivityTypePostToFlickr,
+                                   UIActivityTypePostToVimeo,
+                                   UIActivityTypePostToFacebook,
+                                   UIActivityTypePostToTwitter];
+    
+    activityVC.excludedActivityTypes = excludeActivities;
+    
+    [self presentViewController:activityVC animated:YES completion:nil];
+    
+    
+    
+}
 @end
